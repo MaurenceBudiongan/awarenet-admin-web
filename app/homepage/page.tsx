a7b2d82a9a39290e3b59b7da6b983fb7eef8df6b
Maurence Budiongan <budmau19@gmail.com>
2025-11-06 15:11:29 +0800

    ui improvement

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -4,55 +4,64 @@ import Image from "next/image";
 const Homepage = () => {
   return (
     <div className="flex h-screen">
-      <nav className="w-64 text-white flex flex-col p-6 space-y-6 border-2 border-r-gray-100">
-        <h1 className="text-xl font-bold mb-6 text-green-800">
-          AwareNet Admin
-        </h1>
+      <nav className="w-96 text-white flex flex-col p-6 space-y-6 border-r-3 border-gray-100">
+        <Image
+          src="/logo.png"
+          alt="AwareNet Logo"
+          width={100}
+          height={100}
+          className="rounded-full float-right"
+        />
         <div className="group flex">
           <button className="group-hover:bg-[#05893E] active:bg-[#05893E] w-1 rounded"></button>
-          <button className="group  py-2 px-1 w-px  rounded text-left text-[#05893E] font-inter font-medium">
+          <button className="py-2 px-1 w-px  rounded text-left text-[#05893E] font-inter font-medium">
             Dashboard
           </button>
         </div>
         <div className="group flex">
           <button className="group-hover:bg-[#05893E] w-1 rounded"></button>
-          <button className="  py-2 px-1 w-px  rounded text-left text-[#05893E] font-inter font-medium">
+          <button className="py-2 px-1 w-full  rounded text-left text-[#05893E] font-inter font-medium">
             User
           </button>
         </div>
         <div className="group flex">
           <button className="group-hover:bg-[#05893E] w-1 rounded"></button>
-          <button className="  py-2 px-1 w-px  rounded text-left text-[#05893E] font-inter font-medium">
+          <button className="py-2 px-1 w-full  rounded text-left text-[#05893E] font-inter font-medium">
             Feedback
           </button>
         </div>
         <div className="group flex">
           <button className="group-hover:bg-[#05893E] w-1 rounded"></button>
-          <button className="  py-2 px-1 w-px  rounded text-left text-[#05893E] font-inter font-medium">
+          <button className="py-2 px-1 w-full  rounded text-left text-[#05893E] font-inter font-medium">
             History
           </button>
         </div>
         <div className="group flex">
           <button className="group-hover:bg-[#05893E] w-1 rounded"></button>
-          <button className="  py-2 px-1 w-px  rounded text-left text-[#05893E] font-inter font-medium">
+          <button className="py-2 px-1 w-full  rounded text-left text-[#05893E] font-inter font-medium">
+            Manage Awareness Guide
+          </button>
+        </div>
+
+        <div className="group flex">
+          <button className="group-hover:bg-[#05893E] w-1 rounded"></button>
+          <button className="py-2 px-1 w-full  rounded text-left text-[#05893E] font-inter font-medium">
             Logout
           </button>
         </div>
       </nav>
 
-      <main className="flex flex-col space-y-5 p-8 bg-white-100">
-        <header>
+      <main className="relative flex flex-col space-y-6 w-full">
+        <header className="p-4 border-b-3 border-gray-100 mr-10">
           <Image
-            src="/next.svg" // 🔹 image path (public/logo.png)
-            alt="AwareNet Logo" // 🔹 for accessibility
-            width={40} // 🔹 required width (px)
-            height={40} // 🔹 required height (px)
-            className="rounded-full"
+            src="/profile.png"
+            alt="AwareNet Logo"
+            width={50}
+            height={50}
+            className="rounded-full float-right"
           />
         </header>
-        <p className="bg-green-500 text-green-800 text-xl font-semibold">
-          Welcome to AwareNet Admin homepage
-        </p>
+        <p className="ml-6 text-2xl font-semibold">Dashboard Overview</p>
       </main>
     </div>
   );
