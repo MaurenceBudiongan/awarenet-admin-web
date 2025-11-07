7a4ec3a5a38d5cf9cfecf11413dd05b46b808c26
Maurence Budiongan <budmau19@gmail.com>
2025-11-07 22:46:04 +0800

    table for awarenet user

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,7 +1,41 @@
 "use client";
 
 const User = () => {
-  return <p className="ml-10 mt-4 text-4xl font-bold">User Overview</p>;
+  return (
+    <div>
+      <p className="ml-10 mt-4 text-4xl font-bold">User Overview</p>
+      <table className="table-auto md:table-fixed">
+        <thead>
+          <tr>
+            <th>AUTHENTICATION</th>
+            <th>USER</th>
+            <th>TIMESTAMP</th>
+            <th>ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Google Authentication</td>
+            <td>admin@awarenet.com</td>
+            <td>2025-07-20 10:30 AM</td>
+            <td>delete</td>
+          </tr>
+          <tr>
+            <td>Google Authentication</td>
+            <td>admin@awarenet.com</td>
+            <td>2025-07-20 9:30 AM</td>
+            <td>delete</td>
+          </tr>
+          <tr>
+            <td>AwareNet Authentication</td>
+            <td>user123@awarenet.com</td>
+            <td>2025-07-18 1:00 PM</td>
+            <td>delete</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default User;
