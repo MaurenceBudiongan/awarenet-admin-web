455aefddeb5904c6b24d56812f2a19549cb70876
Maurence Budiongan <budmau19@gmail.com>
2025-11-08 23:46:26 +0800

    feedback ui improvement

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -4,17 +4,17 @@ const Feedback = () => {
   return (
     <div>
       <p className="ml-10 mt-4 text-4xl font-bold">Feedback Overview</p>
-      <div className="flex flex-col">
-        <div className="ml-10 mr-10 mt-10 mb-7 p-4 bg-[#F3F7FC] rounded-xl h-64 flex justify-around gap-y-2">
-          <div className="">
-            <p>Total Feedback Entries</p>
-            <p>9</p>
-            <p>Across all users</p>
+      <div className="flex flex-col ">
+        <div className="ml-10 mr-10 mt-10 mb-7 rounded-xl flex space-x-15 ">
+          <div className="w-full bg-[#eafef3] rounded-lg space-y-4 h-50 flex flex-col justify-center p-10">
+            <p className="text-xl font-semibold ">Total Feedback Entries</p>
+            <p className="text-3xl font-bold text-[#05893E]">9</p>
+            <p className="text-md text-gray-500">Across all users</p>
           </div>
-          <div>
-            <p>Average Rating</p>
-            <p>4.1/5</p>
-            <p>Based on all submissions</p>
+          <div className="w-full bg-[#eafef3] rounded-lg space-y-4 h-50 flex flex-col justify-center p-10">
+            <p className="text-xl font-semibold">Average Rating</p>
+            <p className="text-3xl font-bold text-[#05893E]">4.1 / 5</p>
+            <p className="text-md text-gray-500">Based on all submissions</p>
           </div>
         </div>
       </div>
