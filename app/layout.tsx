42dc42fbc519c29aba98dc78f2936f1fcbc90049
Maurence Budiongan <budmau19@gmail.com>
2025-11-08 14:13:31 +0800

    improve table ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -12,6 +12,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 // export const metadata: Metadata = {
@@ -27,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${geistSans.variable} ${geistMono.variable}  antialiased`}
       >
         {children}
       </body>
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -3,37 +3,47 @@
 const User = () => {
   return (
     <div>
-      <p className="ml-10 mt-4 text-4xl font-bold">User Overview</p>
-      <div className="bg-[#eafef3] p-12 rounded-xl">
-        <table className="w-full text-sm text-left  text-gray-500">
-          <thead className="text-lg  bg-[#e5f6ff]">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                AUTHENTICATION
-              </th>
-              <th>USER</th>
-              <th>TIMESTAMP</th>
-              <th>ACTION</th>
+      <p className="ml-10 mt-4 text-4xl font-bold">AwareNet User</p>
+      <div className=" ml-10 mr-10 mt-10 mb-7 p-4  p-12 rounded-xl">
+        <table className="table-fixed w-full text-sm text-left  text-gray-500">
+          <thead className="text-lg  bg-[#eafef3] rounded-sm">
+            <tr className="">
+              <td className="py-5 px-5">AUTHENTICATION</td>
+              <td className="py-5 px-5">USER</td>
+              <td className="py-5 px-5">TIMESTAMP</td>
+              <td className="py-5 px-5">ACTION</td>
             </tr>
           </thead>
           <tbody className="text-lg">
             <tr className="">
-              <td className="px-6 py-3">Google Authentication</td>
-              <td>admin@awarenet.com</td>
-              <td>2025-07-20 10:30 AM</td>
-              <td>delete</td>
+              <td className="py-5 px-5">Google Authentication</td>
+              <td className="py-5 px-5">admin@awarenet.com</td>
+              <td className="py-5 px-5">2025-07-20 10:30 AM</td>
+              <td className="py-5 px-5">
+                <button className="cursor-pointer bg-white hover:bg-red-400 hover:text-white text-red-400 w-19 h-8 rounded-full border-2 border-red-400">
+                  delete
+                </button>
+              </td>
             </tr>
             <tr>
-              <td className="px-6 py-3">Google Authentication</td>
-              <td>admin@awarenet.com</td>
-              <td>2025-07-20 9:30 AM</td>
-              <td>delete</td>
+              <td className="py-5 px-5 bg-[#fafafb]">Google Authentication</td>
+              <td className="py-5 px-5 bg-[#fafafb]">admin@awarenet.com</td>
+              <td className="py-5 px-5 bg-[#fafafb]">2025-07-20 9:30 AM</td>
+              <td className="py-5 px-5 bg-[#fafafb]">
+                <button className="cursor-pointer bg-white hover:bg-red-400 hover:text-white text-red-400 w-19 h-8 rounded-full border-2 border-red-400">
+                  delete
+                </button>
+              </td>
             </tr>
             <tr>
-              <td className="px-6 py-3">AwareNet Authentication</td>
-              <td>user123@awarenet.com</td>
-              <td>2025-07-18 1:00 PM</td>
-              <td>delete</td>
+              <td className="py-5 px-5">AwareNet Authentication</td>
+              <td className="py-5 px-5">user123@awarenet.com</td>
+              <td className="py-5 px-5">2025-07-18 1:00 PM</td>
+              <td className="py-5 px-5">
+                <button className="cursor-pointer bg-white hover:bg-red-400 hover:text-white text-red-400 w-19 h-8 rounded-full border-2 border-red-400">
+                  delete
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
