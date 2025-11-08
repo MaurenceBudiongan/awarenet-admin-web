9df3f412d0eef4c0d77513307f20306518390829
Maurence Budiongan <budmau19@gmail.com>
2025-11-08 23:23:55 +0800

    feedback content - total feedback ui

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,7 +1,20 @@
 "use client";
 
 const Feedback = () => {
-  return <p className="ml-10 mt-4 text-4xl font-bold">Feedback Overview</p>;
+  return (
+    <div>
+      <p className="ml-10 mt-4 text-4xl font-bold">Feedback Overview</p>
+      <div className="flex flex-col">
+        <div className="ml-10 mr-10 mt-10 mb-7 p-4 bg-[#F3F7FC] rounded-xl h-64 flex justify-around gap-y-2">
+          <div className="">
+            <p>Total Feedback Entries</p>
+            <p>9</p>
+            <p>Across all users</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feedback;
