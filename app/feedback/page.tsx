164b5072a5c74bc882ae1ffc15b455a7814fb700
Maurence Budiongan <budmau19@gmail.com>
2025-11-10 23:47:38 +0800

    turn feedback to responsive

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -5,8 +5,8 @@ const Feedback = () => {
   return (
     <div>
       <p className="ml-10 mt-4 text-4xl font-bold">Feedback Overview</p>
-      <div className="flex flex-col ">
-        <div className="ml-10 mr-10 mt-10  rounded-xl flex space-x-15 ">
+      <div className="ml-10 mr-10 mt-10 space-x-15 space-y-5 flex  flex-col ">
+        <div className="w-full rounded-xl md:flex space-x-15 space-y-5 ">
           <div className="w-full bg-[#eafef3] rounded-lg space-y-4 h-50 flex flex-col justify-center p-10">
             <p className="text-xl font-semibold ">Total Feedback Entries</p>
             <p className="text-3xl font-bold text-[#05893E]">9</p>
@@ -19,8 +19,8 @@ const Feedback = () => {
           </div>
         </div>
 
-        <div className="ml-10 mr-10 mt-5 rounded-xl flex space-x-15 ">
-          <div className="w-full bg-[#e5f6ff] rounded-lg h-50 flex flex-col justify-center p-10">
+        <div className=" rounded-xl space-x-15 grid grid-flow-row grid-cols-3 gap-8 ">
+          <div className="w-full bg-[#e5f6ff] rounded-lg flex-col justify-center p-5 overflow-hidden">
             <div className="flex space-x-4">
               <div>
                 <Image
@@ -95,7 +95,7 @@ const Feedback = () => {
             </div>
           </div>
 
-          <div className="w-full bg-[#e5f6ff] rounded-lg h-50 flex flex-col justify-center p-10">
+          <div className="w-full bg-[#e5f6ff] rounded-lg flex flex-col justify-center p-5 overflow-hidden">
             <div className="flex space-x-4">
               <div>
                 <Image
@@ -170,234 +170,7 @@ const Feedback = () => {
             </div>
           </div>
 
-          <div className="w-full bg-[#e5f6ff] rounded-lg h-50 flex flex-col justify-center p-10">
-            <div className="flex space-x-4">
-              <div>
-                <Image
-                  src="/profile.png"
-                  alt="AwareNet Logo"
-                  width={50}
-                  height={50}
-                  className="rounded-full "
-                />
-              </div>
-              <div>
-                <p className="text-lg">Anora, Rhea</p>
-                <p className="text-md text-[#565d6d]">July 22, 2024</p>
-              </div>
-            </div>
-            <div className="flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-            </div>
-            <div>
-              <p>
-                The new dashboard update is fantastic! Its so much easier to
-                navigate and find the information I need. Great job to the team!
-              </p>
-            </div>
-            <div>
-              <p>
-                <button className="text-[#05893E] float-right">
-                  View Details
-                </button>
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="ml-10 mr-10 mt-5 rounded-xl flex space-x-15 ">
-          <div className="w-full bg-[#e5f6ff] rounded-lg h-50 flex flex-col justify-center p-10">
-            <div className="flex space-x-4">
-              <div>
-                <Image
-                  src="/profile.png"
-                  alt="AwareNet Logo"
-                  width={50}
-                  height={50}
-                  className="rounded-full "
-                />
-              </div>
-              <div>
-                <p className="text-lg">Anora, Rhea</p>
-                <p className="text-md text-[#565d6d]">July 22, 2024</p>
-              </div>
-            </div>
-            <div className="flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-            </div>
-            <div>
-              <p>
-                The new dashboard update is fantastic! Its so much easier to
-                navigate and find the information I need. Great job to the team!
-              </p>
-            </div>
-            <div>
-              <p>
-                <button className="text-[#05893E] float-right">
-                  View Details
-                </button>
-              </p>
-            </div>
-          </div>
-
-          <div className="w-full bg-[#e5f6ff] rounded-lg h-50 flex flex-col justify-center p-10">
-            <div className="flex space-x-4">
-              <div>
-                <Image
-                  src="/profile.png"
-                  alt="AwareNet Logo"
-                  width={50}
-                  height={50}
-                  className="rounded-full "
-                />
-              </div>
-              <div>
-                <p className="text-lg">Anora, Rhea</p>
-                <p className="text-md text-[#565d6d]">July 22, 2024</p>
-              </div>
-            </div>
-            <div className="flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="#EAB308"
-                stroke="#EAB308"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-star-icon lucide-star"
-              >
-                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
-              </svg>
-            </div>
-            <div>
-              <p>
-                The new dashboard update is fantastic! Its so much easier to
-                navigate and find the information I need. Great job to the team!
-              </p>
-            </div>
-            <div>
-              <p>
-                <button className="text-[#05893E] float-right">
-                  View Details
-                </button>
-              </p>
-            </div>
-          </div>
-
-          <div className="w-full bg-[#e5f6ff] rounded-lg h-50 flex flex-col justify-center p-10">
+          <div className="w-full bg-[#e5f6ff] rounded-lg flex flex-col justify-center p-5 overflow-hidden">
             <div className="flex space-x-4">
               <div>
                 <Image
